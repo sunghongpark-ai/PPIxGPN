7f1493484c8fc9be72dc173021372a0cff36e3c6
Sunghong Park <sunghongpark.lab@gmail.com>
2025-04-21 01:08:16 +0900

    Delete RiskPredict.m

--- RiskPredict.m
+++
@@ -1,8 +0,0 @@
-function P = RiskPredict(dataset,parameter,model_param)
-
-Wparam = model_param;
-Wsize = [size(parameter.Uppi);size(parameter.Babt);size(parameter.Bgfa);size(parameter.Bnfl);size(parameter.Btau)];
-[Uppi,Babt,Bgfa,Bnfl,Btau] = ResizeParam(Wparam,Wsize);
-
-Z = (diag(Uppi)+dataset.Lppi)\diag(Uppi)*dataset.Xtest;
-P = (1./(1+exp((-1)*([Babt,Bgfa,Bnfl,Btau]'*Z))))';
