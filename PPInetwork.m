5608decb0c79fc12b6477441d9b5c89a9b902964
Sunghong Park <sunghongpark.lab@gmail.com>
2025-04-21 01:07:47 +0900

    Delete PPInetwork.m

--- PPInetwork.m
+++
@@ -1,8 +0,0 @@
-function L = PPInetwork(network_data)
-
-W = network_data;
-num_protein = size(W,1);
-W = W.*(W>=0.4);
-W(W>0) = 1./(1+exp((-1)*(W(W>0)-mean(W(W>0)))./std(W(W>0))));
-diagW = 1./sqrt(sum(W,2)); diagW(isinf(diagW)==1) = 0;
-L = eye(num_protein)-(diag(diagW)*W*diag(diagW));
