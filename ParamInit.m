68a9b5e5135b3660b193675be40154f9b9f76633
Sunghong Park <sunghongpark.lab@gmail.com>
2025-04-21 01:08:02 +0900

    Delete ParamInit.m

--- ParamInit.m
+++
@@ -1,8 +0,0 @@
-function B = ParamInit(num_protein,mu)
-
-if (mu==0)
-    B = zeros(num_protein,1); szB = size(B);
-    B = (2*rand(szB)-1)*sqrt(6/sum(szB));
-else
-    B = mu*ones(num_protein,1);
-end
